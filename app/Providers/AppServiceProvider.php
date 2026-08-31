c4b0cb831e419fca2ecf9f88ed1250cb9de975f1
shahidkhansedar <shahidkhanasind25@gmail.com>
2026-09-01 01:02:01 +0530

    push

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (config('app.env') === 'production' || isset($_SERVER['HTTPS']) || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         Event::listen(MessageSending::class, function () {
             Log::error('MAIL ATTEMPTED');
         });
